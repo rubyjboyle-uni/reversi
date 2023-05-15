5a6e756d3d5828ba590793f1ef835071708472b5
Ruby <boylerj@uci.edu>
2023-05-15 16:09:35 -0700

    initial push to create the reversi

---
+++ server.js
@@ -0,0 +1,32 @@
+//Set up the static file server
+
+let static = require('node-static');
+
+//Set up the http server
+let http = require('http');
+
+//Assume that we are running on Heroku
+let port = process.env.PORT;
+let directory = __dirname + '/public';
+
+//If we are not on Heroku, then we need to adjust our port and directory
+if((typeof port == 'undefined') || (port === null)) {
+    port = 8080;
+    directory = './public';
+}
+
+//set up static file web server to deliver files from file system
+
+let file = new static.Server(directory);
+
+let app = http.createServer (
+    function(request, response) {
+        request.addListener('end',
+            function(){
+                file.serve(request, response);
+            }
+        ).resume();
+    }
+).listen(port);
+
+console.log('The server is running');
