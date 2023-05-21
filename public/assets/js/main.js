51eedd059e23df5f3ebc0802bd2dc18eb98b3ee3
Ruby <boylerj@uci.edu>
2023-05-20 22:44:26 -0700

    help

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,11 @@
 function getIRIParameterValue(requestedKey) {
     let pageIRI = window.location.search.substring(1);
     let pageIRIVariables = pageIRI.split('&');
-    for(let i = 0; i < pageIRIVariables.length; i++) {
+    for(let i = 0 ; i < pageIRIVariables.length; i++) {
         let data = pageIRIVariables[i].split('=');
         let key = data[0];
         let value = data[1];
-        if(key === requestedKey) {
+        if (key === requestedKey){
             return value;
         }
     }
@@ -13,8 +13,8 @@ function getIRIParameterValue(requestedKey) {
 
 
 let username = getIRIParameterValue('username');
-if((typeof username == 'undefined') || (username === null)){
-    username = "Anonymous_" + Math.floor(Math.random()*1000);
+if ((typeof username == 'undefined') || (username === null)){
+    username = "Anonymous_"+Math.floor(Math.random()*1000);
 }
 
 $('#messages').prepend('<b>'+username+':</b>');
