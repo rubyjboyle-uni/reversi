17091c2c69fdf2cc9950546f427591a92c4aa7d7
Ruby <boylerj@uci.edu>
2023-06-09 22:18:14 -0700

    clean up

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -204,7 +204,7 @@ socket.on('player_disconnected', (payload) => {
         domElements.hide("fade", 500);
     }
 
-    let newHTML = '<p class=\'left_room_response\'>'+ payload.username +' left the '+ payload.room +'. (there are '+ payload.count +' users in this room)</p>';
+    let newHTML = '<p class=\'left_room_response\'>'+ payload.username +' left the chatroom. (There are '+ payload.count +' users in this room)</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
@@ -239,17 +239,18 @@ socket.on('send_chat_message_response', (payload) => {
 })
 
 let old_board = [
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?'],
-    ['?','?','?','?','?','?','?','?']
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' ']
 ];
 
 let my_color = "";
+let interval_timer;
 
 socket.on('game_update', (payload) => {
     if((typeof payload == 'undefined') || (payload === null)){
@@ -369,6 +370,33 @@ socket.on('game_update', (payload) => {
             }
         }
     }
+
+    clearInterval(interval_timer)
+    interval_timer = setInterval(((last_time) => {
+        return ( () => {
+            let d = new Date();
+            let elapsed_m = d.getTime() - last_time;
+            let minutes = Math.floor(elapsed_m / (60 * 1000));
+            let seconds = Math.floor((elapsed_m % (60 * 1000)) / 1000);
+            let total = minutes * 60 + seconds;
+            if (total > 100) {
+                total = 100;
+            }
+            $("#elapsed").css("width", total + "%").attr("aria-valuenow", total);
+            let timestring = "" + seconds;
+            timestring = timestring.padStart(2, '0');
+            timestring = minutes + ":" + timestring;
+            if (total < 100) {
+                $("#elapsed").html(timestring);
+            } else  {
+                $("#elapsed").html("Times up!");
+            }
+        })
+    })(payload.game.last_move_time)
+        , 1000);
+
+
+
 
     $("#whitesum").html(whitesum);
     $("#blacksum").html(blacksum);
